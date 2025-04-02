5425e6ee2b67104987c0be194400ede4e4434ca9
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-02 07:13:07 -0500

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,3 +3,7 @@ import App from './App.vue';
 import router from './router';
 
 createApp(App).use(router).mount('#app');
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
