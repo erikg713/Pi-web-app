7ed4200cb5a294764eb6067b2db3d4e403b1eb8e
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-01 22:09:31 -0500

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -9,3 +9,13 @@ const productSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Product', productSchema);
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  image: String,
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
