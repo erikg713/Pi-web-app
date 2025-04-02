968f0cd07b1aab0645f49a799880a6599b47107f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-01 22:07:44 -0500

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -13,3 +13,12 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+const router = require('express').Router();
+const Product = require('../models/Product');
+
+router.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+module.exports = router;
