708dc39d9ca05c69f3999cc1c58edb68052ef257
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-02 01:42:51 -0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,1 +1,30 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  root: 'src',
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: '../dist',
+    assetsDir: 'assets',
+    minify: 'esbuild',
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
