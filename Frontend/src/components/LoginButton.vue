2958d079f177b25d654c1507ba240d68b955a3c4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-02 07:08:30 -0500

    Update LoginButton.vue

--- Frontend/src/components/LoginButton.vue
+++ Frontend/src/components/LoginButton.vue
@@ -1,1 +1,13 @@
+<template>
+  <button @click="login">Login with Pi</button>
+</template>
 
+<script setup>
+import { authenticateWithPi } from '../pi'
+const emit = defineEmits(['loginSuccess'])
+
+const login = async () => {
+  const user = await authenticateWithPi()
+  if (user) emit('loginSuccess', user)
+}
+</script>
