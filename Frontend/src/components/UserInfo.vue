6ab20afaf9b1eb814c25b14c3e3dc9359ef1eedb
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-02 07:36:07 -0500

    Update UserInfo.vue

--- Frontend/src/components/UserInfo.vue
+++ Frontend/src/components/UserInfo.vue
@@ -1,7 +1,29 @@
 <template>
-  <p v-if="user">Welcome, @{{ user.username }}</p>
+  <div class="user-info">
+    <p v-if="user">Welcome, @{{ user.username }}</p>
+    <p v-else>Welcome, Guest!</p>
+  </div>
 </template>
 
 <script setup>
-defineProps(['user'])
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  user: {
+    type: Object,
+    default: null
+  }
+});
 </script>
+
+<style scoped>
+.user-info {
+  font-size: 1.2em;
+  color: #333;
+  padding: 10px;
+}
+
+.user-info p {
+  margin: 0;
+}
+</style>
