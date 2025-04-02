fc6fe661ef757789d692faf83fd0f6969efb70ed
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-02 07:12:38 -0500

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,1 +1,21 @@
+<template>
+  <div class="app">
+    <Header />
+    <LoginButton @loginSuccess="setUser" />
+    <UserInfo :user="user" />
+    <ProductList />
+  </div>
+</template>
 
+<script setup>
+import { ref } from 'vue'
+import Header from './components/Header.vue'
+import LoginButton from './components/LoginButton.vue'
+import UserInfo from './components/UserInfo.vue'
+import ProductList from './components/ProductList.vue'
+
+const user = ref(null)
+const setUser = (u) => { user.value = u }
+</script>
+
+<style src="./assets/styles.css"></style>
