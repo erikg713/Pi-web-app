2b7a2ee0e60f97b1c53beaeecc1041f40bfcdfda
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-02 01:06:48 -0500

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -22,3 +22,18 @@ function verifyToken(req, res, next) {
 }
 
 module.exports = verifyToken;
+// middleware/auth.js
+module.exports = (req, res, next) => {
+  const auth = { login: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD };
+
+  // Parse login from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  if (login === auth.login && password === auth.password) {
+    return next();
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="Admin Area"');
+  res.status(401).send('Authentication required.');
+};
